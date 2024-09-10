b3d1da4f42aad73586e6b61314f12cbe16a7aef6
유가은 <yga1012@naver.com>
2024-09-10 09:20:26 +0900

    test

---
+++ test/test1.c/test1.c/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  test1.c
+//
+//  Created by 유가은 on 2024/09/10.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
