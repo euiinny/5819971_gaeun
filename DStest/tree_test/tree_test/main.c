ada23c76627cc28236598fbe82b2d9c6622ddb7e
유가은 <yga1012@naver.com>
2024-09-11 12:26:25 +0900

    실습과제-트리

--- DStest/tree_test/tree_test/main.c
+++ DStest/tree_test/tree_test/main.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// TreeNode 구조체 정의
 typedef struct TreeNode {
     int data;
     struct TreeNode* left;
     struct TreeNode* right;
 } TreeNode;
 
-// 배열을 이진 트리로 변환하는 함수 (수정하지 않음)
+
 void GenerateArrayTree(int* tree) {
     int list[] = {1, 2, 9, 3, 5, 10, 13, 4, 6, 7, 8, 11, 12, 14, 15};
     for(int i = 0; i < 15; i++) {
@@ -20,34 +19,31 @@ void GenerateArrayTree(int* tree) {
     printf("\n\n");
 }
 
-// 전위 순회 (배열 인덱스를 사용)
 void ArrayPreOrder(int* tree, int index, int size) {
     if (index >= size || tree[index] == -1) return;
 
-    printf("%d ", tree[index]);  // 루트 노드 출력
-    ArrayPreOrder(tree, 2 * index + 1, size); // 왼쪽 자식 순회
-    ArrayPreOrder(tree, 2 * index + 2, size); // 오른쪽 자식 순회
+    printf("%d ", tree[index]);
+    ArrayPreOrder(tree, 2 * index + 1, size);
+    ArrayPreOrder(tree, 2 * index + 2, size);
 }
 
-// 중위 순회 (배열 인덱스를 사용)
 void ArrayInOrder(int* tree, int index, int size) {
     if (index >= size || tree[index] == -1) return;
 
-    ArrayInOrder(tree, 2 * index + 1, size); // 왼쪽 자식 순회
-    printf("%d ", tree[index]);  // 루트 노드 출력
-    ArrayInOrder(tree, 2 * index + 2, size); // 오른쪽 자식 순회
+    ArrayInOrder(tree, 2 * index + 1, size);
+    printf("%d ", tree[index]);
+    ArrayInOrder(tree, 2 * index + 2, size);
+    
 }
 
-// 후위 순회 (배열 인덱스를 사용)
 void ArrayPostOrder(int* tree, int index, int size) {
     if (index >= size || tree[index] == -1) return;
 
-    ArrayPostOrder(tree, 2 * index + 1, size); // 왼쪽 자식 순회
-    ArrayPostOrder(tree, 2 * index + 2, size); // 오른쪽 자식 순회
-    printf("%d ", tree[index]);  // 루트 노드 출력
+    ArrayPostOrder(tree, 2 * index + 1, size);
+    ArrayPostOrder(tree, 2 * index + 2, size);
+    printf("%d ", tree[index]);
 }
 
-// 트리 순회 함수 (배열 기반)
 void ArrayOrders(int* tree, int size) {
     ArrayPreOrder(tree, 0, size);
     printf("\n");
@@ -57,34 +53,32 @@ void ArrayOrders(int* tree, int size) {
     printf("\n\n");
 }
 
-// 전위 순회
 void LinkPreOrder(TreeNode* node) {
     if (node != NULL) {
-        printf("%d ", node->data);  // 루트 노드 출력
-        LinkPreOrder(node->left);      // 왼쪽 서브트리 순회
-        LinkPreOrder(node->right);     // 오른쪽 서브트리 순회
+        printf("%d ", node->data);
+        LinkPreOrder(node->left);
+        LinkPreOrder(node->right);
     }
 }
 
-// 중위 순회
 void LinkInOrder(TreeNode* node) {
     if (node != NULL) {
-        LinkInOrder(node->left);       // 왼쪽 서브트리 순회
-        printf("%d ", node->data);  // 루트 노드 출력
-        LinkInOrder(node->right);      // 오른쪽 서브트리 순회
+        LinkInOrder(node->left);
+        printf("%d ", node->data);
+        LinkInOrder(node->right);
     }
 }
 
-// 후위 순회
+
 void LinkPostOrder(TreeNode* node) {
     if (node != NULL) {
-        LinkPostOrder(node->left);     // 왼쪽 서브트리 순회
-        LinkPostOrder(node->right);    // 오른쪽 서브트리 순회
-        printf("%d ", node->data);  // 루트 노드 출력
+        LinkPostOrder(node->left);
+        LinkPostOrder(node->right);
+        printf("%d ", node->data);
     }
 }
 
-// 트리 순회 함수 (연결리스트 기반)
+
 void LinkOrders(TreeNode* root) {
     LinkPreOrder(root);
     printf("\n");
@@ -94,7 +88,6 @@ void LinkOrders(TreeNode* root) {
     printf("\n\n");
 }
 
-// 노드를 생성하는 함수
 TreeNode* createNode(int data) {
     TreeNode* newNode = (TreeNode*)malloc(sizeof(TreeNode));
     newNode->data = data;
@@ -103,7 +96,6 @@ TreeNode* createNode(int data) {
     return newNode;
 }
 
-// 배열에서 인덱스를 사용해 이진 트리를 생성하는 함수
 void InsertNode(TreeNode** node, int* list, int index, int size) {
     if (index >= size || list[index] == -1) return;
 
@@ -128,3 +120,4 @@ int main() {
     return 0;
     }
            
+
